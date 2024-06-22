8f563d32d6d77699f3db28e53b582f9691730264
Vitaly <garshin.dev@gmail.com>
2024-06-22 12:27:36 +0300

    change configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: ["@nuxtjs/tailwindcss"],
+  dir: {
+    pages: './src/app/routes',
+    layouts: './src/app/layouts',
+  },
+  alias: {
+    '~': '/src'
+  }
+  // srcDir: 'src/'
 })
