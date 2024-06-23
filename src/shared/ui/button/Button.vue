5c871462b8b488616e0af58aa24cf8a477faf3b1
Vitaly <garshin.dev@gmail.com>
2024-06-23 11:45:42 +0300

    add templates for ui elements

---
+++ src/shared/ui/button/Button.vue
@@ -0,0 +1,11 @@
+<template>
+	<button>
+		<slot />
+	</button>
+</template>
+
+<script setup lang="ts">
+interface ButtonProps {
+
+}
+</script>
---
+++ src/shared/ui/input/Input.vue
@@ -0,0 +1,11 @@
+<template>
+	<label for="">
+		<input type="text">
+	</label>
+</template>
+
+<script setup lang="ts">
+interface InputProps {
+
+}
+</script>
