ada94d235d3d1711882c98f8634de0c74b3d333e
Vitaly Garshin <garshin.dev@gmail.com>
2024-06-24 19:57:33 +0300

    add new color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,17 @@
 import type { Config } from 'tailwindcss'
 
-export default <Partial<Config>> {
-	future: {
-		hoverOnlyWhenSupported: true
-	},
-	theme: {
-		colors: {
-			white: '#fff',
-			black: '#000',
-			blue: '#1258E3',
-			gray: '#a2a2a2',
-		}
-	},
-	content: ['./src/**/*.{vue, ts}'],
+export default <Partial<Config>>{
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  theme: {
+    colors: {
+      white: '#fff',
+      black: '#000',
+      blue: '#1258E3',
+      gray: '#a2a2a2',
+      transparent: 'transparent',
+    },
+  },
+  content: ['./src/**/*.{vue, ts}'],
 }
