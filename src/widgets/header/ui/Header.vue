6dc11e74bf4008032601468e2e53c57589500c56
Vitaly Garshin <garshin.dev@gmail.com>
2024-06-24 19:58:15 +0300

    change links

--- src/widgets/header/ui/Header.vue
+++ src/widgets/header/ui/Header.vue
@@ -1,57 +1,51 @@
 <template>
-	<header class="py-2 px-4 flex items-center justify-between text-blt ue shadow-md">
-		<nav>
-			<ul class="flex items-center gap-2">
-				<li
-					v-for="el in navLinks"
-					:key="el"
-				>
-					<NuxtLink :to="el.link" class="uppercase bold transition duration-300 border-b-blue border-b hover:border-none">
-						{{ $t(el.name) }}
-					</NuxtLink>
-				</li>
-			</ul>
-		</nav>
-		<div>
-			<ul class="flex items-center gap-2">
-				<li>
-					<button>
-						{{ $t('sign_in') }}
-					</button>
-				</li>
-				<li>
-					<button>
-						{{ $t('sign_up') }}
-					</button>
-				</li>
-			</ul>
-		</div>
-		<div>
-			<ul class="flex flex-col gap-1">
-				<li
-					v-for="locale in locales"
-					:key="locale"
-				>
-					<button @click="setLocale(locale.code)">
-						{{ locale.name }}
-					</button>
-				</li>
-			</ul>
-		</div>
-	</header>
+  <header class="text-blt ue flex items-center justify-between px-4 py-2 shadow-md">
+    <nav>
+      <ul class="flex items-center gap-2">
+        <li v-for="el in navLinks" :key="el">
+          <NuxtLink :to="el.link" class="bold uppercase">
+            {{ $t(el.name) }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <div>
+      <ul class="flex items-center gap-2">
+        <li>
+          <button>
+            {{ $t('sign_in') }}
+          </button>
+        </li>
+        <li>
+          <button>
+            {{ $t('sign_up') }}
+          </button>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <ul class="flex flex-col gap-1">
+        <li v-for="locale in locales" :key="locale">
+          <button @click="setLocale(locale.code)">
+            {{ locale.name }}
+          </button>
+        </li>
+      </ul>
+    </div>
+  </header>
 </template>
 
 <script setup lang="ts">
 const { locales, setLocale } = useI18n()
 
 const navLinks = [
-	{
-		name: 'home',
-		link: '/'
-	},
-	{
-		name: 'personal',
-		link: '/personal'
-	}
+  {
+    name: 'home',
+    link: '/',
+  },
+  {
+    name: 'personal',
+    link: '/personal',
+  },
 ]
 </script>
