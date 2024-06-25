84d94b4232a4ccbefd13c522212649a567d08b6b
Vitaly Garshin <garshin.dev@gmail.com>
2024-06-25 23:08:18 +0300

    change nuxt.config, add path for ui kit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,26 @@ import i18nConfig from './i18n.config'
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint", "@pinia/nuxt", "@nuxtjs/i18n"],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@pinia/nuxt', '@nuxtjs/i18n'],
   css: ['~/shared/assets/styles/main.css'],
   dir: {
     pages: './src/app/routes',
     layouts: './src/app/layouts',
     public: './src/app/public',
   },
+  components: {
+    dirs: [
+      {
+        path: './src/shared/ui',
+        pathPrefix: false,
+      },
+    ],
+  },
   alias: {
-    '~': '/src'
+    '~': '/src',
   },
   eslint: {
     checker: true,
   },
-  i18n: i18nConfig
+  i18n: i18nConfig,
 })
