1174033212ec917e782806bc6049b944fbdd5fab
Vitaly Garshin <garshin.dev@gmail.com>
2024-06-26 23:35:05 +0300

    add middleware for router

--- server/router/index.js
+++ server/router/index.js
@@ -1,5 +1,6 @@
 import Router from 'express'
 import userController from '../controllers/user.controller.js'
+import authMiddleware from '../middleware/auth.middleware.js'
 
 const router = new Router()
 
@@ -8,6 +9,6 @@ router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 router.get('/refresh', userController.refresh)
 router.get('/posts', userController.getPosts)
-router.get('/users', userController.getUsers)
+router.get('/users', authMiddleware, userController.getUsers)
 
 export default router
